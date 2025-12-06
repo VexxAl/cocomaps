3179a67a33353d9201fb49f6c590e93370282526
Valentín <valentinalderete19@gmail.com>
2025-12-05 23:41:23 -0300

    style: Update HTML structure and improve CSS variable naming for clarity

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,9 +7,12 @@ function SearchBar({ onSearch }) {
   return (
     <div id='buscador'>
       <input 
+        id="search-input"
+        name="search"
         className="search-input"
         type="text" 
         placeholder="Buscar comedor o dirección..." 
+        autoComplete="off"
         onChange={(e) => onSearch(e.target.value)} // Preparamos para lógica futura
       />
     </div>
